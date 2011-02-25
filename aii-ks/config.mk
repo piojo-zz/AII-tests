a5f89f0b481216527feb0ebb6d27caa2524ac7f5
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2011-02-25 16:37:32 +0100

    Import various changes from upstream
    - Use CAF::FileWriter to update kickstart config files
    - Add support for the "--driveorder" and "--append" options of the
      "bootloader" kickstart command
    - Add support for the "ignoredisk" kickstart command
    - Static network configuration during install
    - MD RAID booting fixes
    - LVM fixes during reinstall
    - Support for ncm-spma v2.0 schema

--- aii-ks/config.mk
+++ aii-ks/config.mk
@@ -4,10 +4,10 @@ AUTHOR=Luis Fernando Muñoz Mejías
 MAINTAINER=Luis Fernando Muñoz Mejías
 DESCRIPTION=AII plug-in that generates Kickstart files.
 DESCR=$(DESCRIPTION)
-VERSION=1.1.16
+VERSION=1.1.32
 RELEASE=1
 PAN_PATH_DEV=/system/blockdevices/
 PAN_PATH_FS=/system/filesystems/
 PACKAGE_PATH=NCM
-NCM_EXTRA_REQUIRES=ncm-lib-blockdevices >= 0.15 aii-server >= 0.99 pan-templates > 3.0.7
-DATE=03/07/08 20:35
+NCM_EXTRA_REQUIRES=ncm-lib-blockdevices >= 0.18.3 aii-server >= 0.99 pan-templates > 3.0.7 ccm >= 2.1.0 perl-CAF >= 1.7.0
+DATE=13/01/11 17:08
