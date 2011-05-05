5cd0ab9f2af23f571c3270ce30d8ba6e8db467d1
Gabor Gombas <Gabor.Gombas@morganstanley.com>
2011-05-05 13:39:18 +0200

    Import r2596 from upstream SVN
    2011-02-27  Michel Jouvin
    
      * Release: 1.1.33
      * Fix --driveorders value produced by ks.pm
      * TPL/config.tpl: improved default definition of list of disks to ignore
        or whose partitions must be cleared (ensure all disks defined are in one
        of the list)
      * TPL/config.tpl: by default, define bootdisk_order with the disks having boot=true, if any

--- aii-ks/config.mk
+++ aii-ks/config.mk
@@ -4,10 +4,10 @@ AUTHOR=Luis Fernando Muñoz Mejías
 MAINTAINER=Luis Fernando Muñoz Mejías
 DESCRIPTION=AII plug-in that generates Kickstart files.
 DESCR=$(DESCRIPTION)
-VERSION=1.1.32
+VERSION=1.1.33
 RELEASE=1
 PAN_PATH_DEV=/system/blockdevices/
 PAN_PATH_FS=/system/filesystems/
 PACKAGE_PATH=NCM
 NCM_EXTRA_REQUIRES=ncm-lib-blockdevices >= 0.18.3 aii-server >= 0.99 pan-templates > 3.0.7 ccm >= 2.1.0 perl-CAF >= 1.7.0
-DATE=13/01/11 17:08
+DATE=27/02/11 12:30
