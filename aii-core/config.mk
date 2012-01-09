a09d5fdc8f05f110137835e4f87c1c542ac6dc3a
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-01-09 17:45:29 +0100

    Integrate the PXELinux generator and the core AII

---
+++ aii-core/config.mk
@@ -0,0 +1,20 @@
+#
+# configuration values for AII
+#
+
+COMP=aii
+
+NAME=$(COMP)
+DESCR=AII is the Automated Installation Infrastructure
+VERSION=2.4.6
+RELEASE=1
+
+AUTHOR=Luis Fernando Muñoz Mejías <luisf.munnoz@uam.es>, Ronald Starink <ronalds@nikhef.nl>, Enrico Ferro, Cristina Aiftimei, Rafael Garcia, Cesar Lobo
+
+MAINTAINER=Ronald Starink <ronalds@nikhef.nl>, Luis Fernando Muñoz Mejías <luisf.munnoz@uam.es>, Michel Jouvin <jouvin@lal.in2p3.fr>
+
+CONFIGFILE= $(QTTR_ETC)/aii-shellfe.conf
+
+TESTVARS=
+
+DATE=24/06/11 17:41
---
+++ aii-pxelinux/config.mk
@@ -0,0 +1,10 @@
+COMP=pxelinux
+NAME=aii-$(COMP)
+AUTHOR=Luis Fernando Muñoz Mejías
+MAINTAINER=Luis Fernando Muñoz
+DESCRIPTION=AII plug-in for configuring pxelinux.
+DESCR=$(DESCRIPTION)
+VERSION=1.1.12
+RELEASE=1
+NCM_EXTRA_REQUIRES= aii-server >= 0.99 aii-ks >= 1.0.7 perl-CAF >= 1.7.0
+DATE=05/01/12 19:24
